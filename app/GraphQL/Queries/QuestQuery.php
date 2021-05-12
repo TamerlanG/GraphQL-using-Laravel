94329888a51e2c97a1408e9a964ffac974ba7102
Tamerlan Gudabyev <tamerlanalbehairy@gmail.com>
2021-05-12 13:42:56 +0600

    feat: graphql queries

---
+++ app/GraphQL/Queries/QuestQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\GraphQL\Queries;
+
+
+use App\Models\Quest;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class QuestQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'quest',
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Quest');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::int(),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        return Quest::findOrFail($args['id']);
+    }
+}
---
+++ app/GraphQL/Queries/QuestsQuery.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\GraphQL\Queries;
+
+
+use App\Models\Quest;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class QuestsQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'quests',
+    ];
+
+    public function type(): Type
+    {
+        return Type::listOf(GraphQL::type('Quest'));
+    }
+
+    public function resolve($root, $args)
+    {
+        return Quest::all();
+    }
+}
+
