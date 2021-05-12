9730238ea12296c7f1238268388a5b07fe76371b
Tamerlan Gudabyev <tamerlanalbehairy@gmail.com>
2021-05-12 13:44:11 +0600

    chore: ide-helper and fillables

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Quest[] $quests
+ * @property-read int|null $quests_count
+ * @method static \Database\Factories\CategoryFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Category extends Model
 {
     use HasFactory;
--- app/Models/Quest.php
+++ app/Models/Quest.php
@@ -5,9 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Quest
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property int $reward
+ * @property int $category_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category $category
+ * @method static \Database\Factories\QuestFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest whereReward($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Quest whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Quest extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'category_id', 'description', 'reward'];
 
     public function category(){
         return $this->belongsTo(Category::class);
